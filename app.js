25bc897279369a9b140b4841c9b911de9c9f7c73
lucahralaivelona <111045269+lucahralaivelona@users.noreply.github.com>
2024-08-21 20:29:53 +0300

    app.js

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ import { cron } from "https://deno.land/x/deno_cron/cron.ts";
 
 const APP_URLS = [
     "https://bibila.vercel.app",
-    "https://gestionlocationvoiture.onrender.com",
-    "https://visiteur.onrender.com",
+    "https://gestionlocationvoiture.onrender.com/api/v1/Rented",
+    "https://visiteur.onrender.com/api/v1/Visiteurs",
     "https://ui-visiteur.vercel.app",
 ];
 
 cron("* * * * *", async () => {  // Toutes les minutes
     for (const url of APP_URLS) {
-        await fetch(url);
-        console.log("ping reussie")
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                console.error(`Erreur lors du ping de ${url}: ${response.statusText}`);
+                continue;
+            }
+
+            const data = await response.text(); // Vous pouvez utiliser .json() si vous attendez un JSON
+            console.log(`Réponse reçue de ${url}: ${data}`);
+        } catch (error) {
+            console.error(`Erreur lors de la requête à ${url}:`, error);
+        }
     }
 });
